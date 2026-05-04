acbb71e4b134fd911951382ce186192e90acdbfe
Musbi <musbi700@gmail.com>
2026-05-04 18:34:21 +0000

    added home

--- app/routers/home.py
+++ app/routers/home.py
@@ -4,6 +4,8 @@ from app.schemas import HomeResponse
 
 router = APIRouter(tags=["Home"])
 
+#testing why the home route is not working, it should return a simple welcome message and timestamp
+
 @router.get("/", response_model=HomeResponse)
 async def home():
     return HomeResponse(message="Welcome to the Fire Alert System API!", timestamp=datetime.utcnow().isoformat())
